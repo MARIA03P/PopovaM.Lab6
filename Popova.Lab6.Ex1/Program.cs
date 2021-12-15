edbc353d7b6ef28719c07fcd65b9566808b749a8
arinameliakina <melyakina2003@mail.ru>
2021-12-16 00:02:34 +0300

    Добавьте файлы проекта.

---
+++ Popova.Lab6.Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Popova.Lab6.Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("вводим переменную n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] ar = new int[n];
+
+            Random r = new Random();
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = r.Next(-10, 10);
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Console.WriteLine(ar[i]);
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] % 2 == 0) ;
+                sum += ar[i];
+            }
+
+            Console.WriteLine($"Sum = {sum}");
+        }
+    }
+}
---
+++ Popova.Lab6.Ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Popova.Lab6.Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введем переменную n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] ar = new int[n, n];
+
+            Random r = new Random();
+
+            for (int i = 0; i < ar.GetLength(0); i++)
+            {
+                for (int j = 0; j < ar.GetLength(1); j++)
+                {
+                    ar[i, j] = r.Next(-10, 10);
+                }
+
+            }
+
+            for (int i = 0; i < ar.GetLength(0); i++)
+            {
+                for (int j = 0; j < ar.GetLength(1); j++)
+                {
+                    Console.Write(ar[i, j] + "\t");
+                }
+                Console.WriteLine();
+        
+            }   
+        }
+    }
+}
